5e31aedeff1edd14ca5643b6baa1432cde72b269
Binhanh <binhanh080103@gmail.com>
2022-11-02 20:10:56 +0700

    complete Navbar and Boardbar

--- client/src/components/BoardBar/BoardBar.jsx
+++ client/src/components/BoardBar/BoardBar.jsx
@@ -1,11 +1,47 @@
 import React from "react";
 import './BoardBar.scss'
+import { IoMdCafe } from 'react-icons/io'
+import { FiMoreHorizontal } from 'react-icons/fi'
 
 const BoardBar = () => {
   return (
     <>
         <div className="board-bar">
-            <p>Board bar</p>
+            <div className="board-bar__left">
+              <div className="btn-tag hover">
+                <IoMdCafe size={20} />
+                <p>BanhTheCake</p>
+              </div>
+              <div className="separation"></div>
+              <div className="btn-tag hover">
+                <p>private workspace</p>
+              </div>
+              <div className="separation"></div>
+              <div className="member-list">
+                <div className="img-wrapper">
+                  <img src="https://images.unsplash.com/photo-1607305387299-a3d9611cd469?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
+                </div>
+                <div className="img-wrapper">
+                  <img src="https://images.unsplash.com/photo-1607305387299-a3d9611cd469?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
+                </div>
+                <div className="img-wrapper">
+                  <img src="https://images.unsplash.com/photo-1607305387299-a3d9611cd469?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
+                </div>
+                <div className="img-wrapper">
+                  <img src="https://images.unsplash.com/photo-1607305387299-a3d9611cd469?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
+                </div>
+                <p className="rest-member">+7</p>
+              </div>
+              <div className="btn-tag hover">
+                <p>Invite</p>
+              </div>
+            </div>
+            <div className="board-bar__right">
+              <div className="btn-tag hover">
+                <FiMoreHorizontal size={20} />
+                <p>Show more</p>
+              </div>
+            </div>
         </div>
     </>
   );
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,48 @@
 import React from "react";
 import './Navbar.scss'
+import { CgMenuGridR } from 'react-icons/cg'
+import { IoSearch } from 'react-icons/io5'
+import { IoMdNotificationsOutline } from 'react-icons/io'
+import { BsHouse, BsClipboardData, BsInfoCircleFill } from 'react-icons/bs'
+import { AiOutlinePlusSquare } from 'react-icons/ai'
 
 const Navbar = () => {
   return (
     <>
         <div className="Navbar">
-          <p>App bar</p>
+          <div className="Navbar-left">
+            <div className="btn-tag hover">
+              <CgMenuGridR size={23} />
+            </div>
+            <div className="btn-tag hover">
+              <BsHouse size={23} />
+            </div>
+            <div className="btn-tag hover">
+              <BsClipboardData size={23} />
+              <p>Boards</p>
+            </div>
+            <div className="btn-tag">
+              <input className="input" type="text" placeholder="Jump to... " spellCheck={false}/>
+              <IoSearch size={23} />
+            </div>
+          </div>
+          <div className="Navbar-center">
+            <p>BanhTheTomato</p>
+          </div>
+          <div className="Navbar-right">
+          <div className="btn-tag hover">
+              <AiOutlinePlusSquare size={23} />
+            </div>
+            <div className="btn-tag hover">
+              <BsInfoCircleFill size={18} />
+            </div>
+            <div className="btn-tag hover">
+              <IoMdNotificationsOutline size={23} />
+            </div>
+            <div className="avatar">
+              <img src="https://images.unsplash.com/photo-1607305387299-a3d9611cd469?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
+            </div>
+          </div>
       </div>
     </>
   );
